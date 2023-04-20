846ecc95142afa1131bf9e885aa59602302325d4
Robson Martinhão <robmartinhao@gmail.com>
2023-04-20 20:31:22 -0300

    5.1. Prototipando a pesquisa de produtos

--- src/main/java/com/algaworks/Main.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -0,0 +1,24 @@
+package com.algaworks.pedidovenda.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaProdutosBean {
+
+    private List<Integer> produtosFiltrados;
+
+    public PesquisaProdutosBean() {
+        produtosFiltrados = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            produtosFiltrados.add(i);
+        }
+    }
+
+    public List<Integer> getProdutosFiltrados() {
+        return produtosFiltrados;
+    }
+}
