0ef8424037c65da5da772a9a650861b41e06c697
Robson Martinhão <robmartinhao@gmail.com>
2023-05-06 13:52:06 -0300

    10.6. Criando entidades do pedido de venda

---
+++ src/main/java/com/algaworks/pedidovenda/model/ItemPedido.java
@@ -0,0 +1,68 @@
+package com.algaworks.pedidovenda.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class ItemPedido implements Serializable {
+
+    private Long id;
+    private Integer quantidade;
+    private BigDecimal valorUnitario;
+    private Produto produto;
+
+    private Pedido pedido;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemPedido that = (ItemPedido) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/algaworks/pedidovenda/model/StatusPedido.java
@@ -0,0 +1,5 @@
+package com.algaworks.pedidovenda.model;
+
+public enum StatusPedido {
+    ORCAMENTO, EMITIDO, CANCELADO
+}
