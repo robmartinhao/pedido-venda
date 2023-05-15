d3bc84fe53288a43b07e1147bc946361bfca2f13
Robson Martinhão <robmartinhao@gmail.com>
2023-05-15 14:48:05 -0300

    12.1. Criando um produtor CDI para injetar EntityManager

--- src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,16 +1,28 @@
 package com.algaworks.pedidovenda.controller;
 
+import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @ViewScoped
 public class CadastroProdutoBean implements Serializable {
 
+    @Inject
+    private EntityManager manager;
     private Produto produto;
+    private List<Categoria> categoriasRaizes;
+
+    public void inicializar() {
+        System.out.println("Inicializando...");
+        categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+    }
 
     public CadastroProdutoBean() {
         this.produto = new Produto();
@@ -22,5 +34,9 @@ public class CadastroProdutoBean implements Serializable {
 
     public Produto getProduto() {
         return produto;
+    }
+
+    public List<Categoria> getCategoriasRaizes() {
+        return categoriasRaizes;
     }
 }
