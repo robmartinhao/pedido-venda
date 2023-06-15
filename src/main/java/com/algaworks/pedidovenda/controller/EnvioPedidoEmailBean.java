0dd617fb5f026977fe6bf008aa753ca509f76e5a
Robson Martinhão <robmartinhao@gmail.com>
2023-06-15 13:30:30 -0300

    14.4. Template de e-mail com Apache Velocity

--- src/main/java/com/algaworks/pedidovenda/controller/EnvioPedidoEmailBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/EnvioPedidoEmailBean.java
@@ -4,11 +4,15 @@ import com.algaworks.pedidovenda.model.Pedido;
 import com.algaworks.pedidovenda.util.jsf.FacesUtil;
 import com.algaworks.pedidovenda.util.mail.Mailer;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
+import org.apache.velocity.tools.generic.NumberTool;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Locale;
+import java.util.Objects;
 
 @Named
 @ViewScoped
@@ -25,7 +29,11 @@ public class EnvioPedidoEmailBean implements Serializable {
 
         message.to(pedido.getCliente().getEmail())
                 .subject("Seu pedido de venda")
-                .bodyHtml("<strong>Valor total:</strong> " + this.pedido.getValorTotal())
+                .bodyHtml(new VelocityTemplate(Objects.requireNonNull(getClass()
+                        .getResourceAsStream("/emails/pedido.template")).toString()))
+                .put("pedido", this.pedido)
+                .put("numberTool", new NumberTool())
+                .put("locale", new Locale("pt", "BR"))
                 .send();
 
         FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso!");
